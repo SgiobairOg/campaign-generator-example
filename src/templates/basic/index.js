4aea22def813c605a3be7ea49a640a4199d8959e
Jason Wilson <jason@wilsons.io>
2019-01-24 10:32:07 +0100

    refactor(template): Update structure of templates directory

--- src/templates/basic/index.js
+++ src/templates/basic/index.js
@@ -8,7 +8,10 @@ provider.setLocale(process.env.LOCALE || 'en');
 const writePage = function() {
     return `<!DOCTYPE html>
         <html lang='${provider.getLocale()}'>
-            <body>
+            <head>
+                <title>${provider.translate('meta.title')}</title>
+            </head>
+            <body class='${provider.translate('styles.bodyClass')}'>
                 <h1>${provider.translate('greeting.normal')}</h1>
                 <p>${provider.translateN('%s dog', '3')}</p>
             </body>
